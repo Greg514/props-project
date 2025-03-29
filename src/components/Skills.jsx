3e72d6048ef16f43940a2a1ea6dc6a0311ac0685
gregory <jeanlouisgregory21@gmail.com>
2025-03-29 18:13:35 -0400

    added the key for the ul child and better code

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,22 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Skills() {
-const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express", "MongoDB", "Figma"];
+  const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Express", "MongoDB", "Figma"];
 
-const listOfSkill = skills.map(skills => <ul>{skills}</ul>);
-{
   return (
-<>
-
-  <div className="skills">
-    <h1 className="skills-name">Skills</h1>
-    <ul className="skills-list">
-      {listOfSkill}
-    </ul>
-  </div>
-
-  
-</>
-  )
-}
+    <div className="skills">
+      <h1 className="skills-name">Skills</h1>
+      <ul className="skills-list">
+        {skills.map((skill, i) => (
+          <li key={i}>{skill}</li> 
+        ))}
+      </ul>
+    </div>
+  );
 }
